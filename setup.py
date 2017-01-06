368de4d82fba29afed94603fef8c68919fb039b8
Chris Coetzee <chriscz93@gmail.com>
2017-01-06 22:09:01 +0200

    Update MANIFEST and setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-webpack-helper",
-    version="0.1.2",
+    version="0.1.3",
     url="httpd://github.com/chriscz/django-webpack-helper",
     description="A django app that helps integrate webpack",
     author="Chris Coetzee",
     license='Mozilla Public License 2.0 (MPL 2.0)',
     author_email='chriscz93@gmail.com',
-    packages=find_packages(include=['webpack_helper']),
+    packages=find_packages(exclude=['exampleapp']),
     install_requires=[
         'django-webpack-loader'
     ],
